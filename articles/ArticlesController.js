497b50daff849194922269d91ad8785095bff0d7
LianMiranda <lianmiranda26@gmail.com>
2024-12-17 16:33:58 -0400

    [FEAT] Excluir e editar categorias + Formulario de artigo com TinyMCE e salvamento

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const categoryModel = require("../categories/CategoryModel")
+const articleModel = require("./ArticleModel")
+const slugify = require("slugify")
 
-router.get('/articles', (req, res) => {
+router.get('/admin/articles', (req, res) => {
     res.send('PAGINA DE ARTIGOS')
 })
 
 router.get('/admin/articles/new', (req, res) => {
-    res.send('PAGINA PARA CRIAR NOVOS ARTIGOS')
+    categoryModel.findAll().then(categories => {
+        res.render('admin/articles/newArticle', {categories: categories})
+    })
+})
+
+router.post("/articles/save", (req, res) => {
+    var title = req.body.articleTitle;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    if (!title || typeof title !== "string" || title.trim() === "") {
+        return res.status(400).send("O título do artigo é obrigatório!.");
+    }
+
+    articleModel.create({
+        title: title,
+        slug: slugify(title.toString()),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles")
+    })
+
+
 })
 
 module.exports = router
--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const Category = require('./CategoryModel');
 const router = express.Router();
-const slugify = require('slugify')
+const slugify = require('slugify');
+const { where } = require('sequelize');
 
 router.get("/admin/categories/new", (req, res) => {
     res.render('admin/categories/newCategory')
@@ -17,9 +18,60 @@ router.post("/categories/save", (req, res) => {
             title: category,
             slug: slugify(category)
         }).then(() => {
-            res.redirect("/")
+            res.redirect("/admin/categories")
         })
     }
+})
+
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id;
+
+    if(id){
+        if(!isNaN(id)){
+            Category.destroy({
+                where:{
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            })
+        }else{
+            res.redirect("/admin/categories")
+        }
+    }else{
+        res.redirect("/admin/categories")
+    }
+})
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/categories");
+    }
+
+    Category.findByPk(id).then(category => {
+        if(category){
+            res.render("admin/categories/editCategory", {category: category})
+        }else{
+            res.redirect("/admin/categories");
+        }
+    }).catch(err => {
+        res.redirect("/admin/categories");
+    })
+})
+
+router.post('/categories/update', (req,res) => {
+    var id = req.body.id
+    var category = req.body.category
+
+    Category.update({title: category,slug: slugify(category)}, {
+        where:{
+            id: id
+        }
+    }).then(() => {
+        res.redirect('/admin/categories')
+    })
 })
 
 router.get('/admin/categories', (req, res) =>{
